a9df56deaf1b773a612f503e27abfb7b722eadc9
Ikmal Azman <opikmalazman@gmail.com>
2023-03-30 08:26:54 +0100

    finish UI for multiple image selection

--- SwiftUI-PHPicker/ContentView.swift
+++ SwiftUI-PHPicker/ContentView.swift
@@ -8,31 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var image : Image = Image(systemName: "photo.artframe")
+    @State var shouldShowPhotoPicker : Bool = false
+    @State var selectedImages = [UIImage]()
     
     var body: some View {
-        
         VStack {
-            Spacer()
-            
-            image
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(maxWidth: .infinity, maxHeight:150)
-            
-            Spacer()
-            
-            Button {
-                
-            } label: {
-                Label("Photo Library", image: "")
-                    .padding()
-                    .frame(maxWidth: .infinity)
+            if selectedImages.isEmpty {
+                placeholderImage
+            } else {
+                selectedImage
             }
-            .buttonStyle(.borderedProminent)
             
+            photosLibraryButton
+                .padding(.top)
         }
         .padding()
+        .sheet(isPresented: $shouldShowPhotoPicker) {
+            PhotoPicker(selectedImages: $selectedImages)
+        }
+    }
+    
+    var placeholderImage : some View {
+        Image(systemName: "photo.artframe")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(maxWidth: .infinity, maxHeight:150)
+    }
+    
+    var selectedImage : some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(selectedImages, id: \.self) { image in
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 150, maxHeight: 150)
+                }
+            }
+        }
+    }
+    
+    var photosLibraryButton : some View {
+        Button {
+            shouldShowPhotoPicker.toggle()
+        } label: {
+            Label("Photo Library", image: "")
+                .padding()
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.borderedProminent)
     }
 }
 
