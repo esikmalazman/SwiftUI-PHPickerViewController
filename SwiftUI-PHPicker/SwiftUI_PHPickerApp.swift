9508a7a943043317c1587de8616da1cf3bc8a1fc
Ikmal Azman <opikmalazman@gmail.com>
2023-03-30 06:28:57 +0100

    Initial Commit

---
+++ SwiftUI-PHPicker/SwiftUI_PHPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PHPickerApp.swift
+//  SwiftUI-PHPicker
+//
+//  Created by Ikmal Azman on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PHPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
